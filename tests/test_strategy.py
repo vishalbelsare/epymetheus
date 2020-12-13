eb8967981a47337e39fcf70dc637f6a1eec6d1f7
Shota Imaki <shota.imaki@icloud.com>
2020-12-13 14:30:29 +0900

    Add tests

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -6,7 +6,6 @@ from epymetheus import Strategy
 
 
 class MyStrategy(Strategy):
-
     def __init__(self, param_1, param_2):
         self.param_1 = param_1
         self.param_2 = param_2
@@ -40,3 +39,21 @@ class TestStrategy:
         universe = None
 
         assert strategy(universe) == [1.0 * trade("A"), 2.0 * trade("B")]
+
+    def test_get_params(self):
+        strategy = create_strategy(self.my_strategy, param_1=1.0, param_2=2.0)
+        assert strategy.get_params() == {"param_1": 1.0, "param_2": 2.0}
+
+        strategy = MyStrategy(param_1=1.0, param_2=2.0)
+        assert strategy.get_params() == {}
+
+    def test_set_params(self):
+        strategy = create_strategy(self.my_strategy, param_1=1.0, param_2=2.0)
+        assert strategy.get_params() == {"param_1": 1.0, "param_2": 2.0}
+        strategy.set_params(param_1=3.0)
+        assert strategy.get_params() == {"param_1": 3.0, "param_2": 2.0}
+
+    def test_warn(self):
+        strategy = create_strategy(self.my_strategy, param_1=1.0, param_2=2.0)
+        with pytest.raises(DeprecationWarning):
+            strategy.evaluate(None)
