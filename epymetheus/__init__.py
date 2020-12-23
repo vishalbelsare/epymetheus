52257d1b19e76760d81cd4872ef6f8dac3743acc
Shota Imaki <shota.imaki@icloud.com>
2020-12-23 20:40:44 +0900

    [REF] Abolish `Wealth` (close #109)

--- epymetheus/__init__.py
+++ epymetheus/__init__.py
@@ -6,7 +6,6 @@ from epymetheus.strategy import create_strategy
 from epymetheus.trade import Trade
 from epymetheus.trade import trade
 from epymetheus.universe import Universe
-from epymetheus.wealth import Wealth
 
 from . import datasets
 from . import utils
