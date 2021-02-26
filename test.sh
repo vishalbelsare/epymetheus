3ae3a55f95195d5991afcf3b69714fa1dbc890de
Shota Imaki <shota.imaki.0801@gmail.com>
2021-02-26 12:02:45 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,7 @@ python3 -m pytest --doctest-modules epymetheus
 python3 -m pytest --doctest-modules tests
 
 python3 -m flake8 epymetheus
-python3 -m black --check epymetheus || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black epymetheus
-python3 -m isort --check --force-single-line-imports epymetheus || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports epymetheus
+python3 -m black --check --quiet epymetheus || read -p "Run black? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m black --quiet epymetheus
+python3 -m isort --check --force-single-line-imports epymetheus || read -p "Run isort? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports --quiet epymetheus
+python3 -m black --quiet tests
+python3 -m isort --force-single-line-imports --quiet tests
